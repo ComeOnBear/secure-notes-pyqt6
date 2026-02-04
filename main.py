a15ab95ab63caae76ab2a3fcddce16f52b5fc00c
Michael Royston-Hayes <Michael.Royston-Hayes.2025@live.rhul.ac.uk>
2026-02-04 11:21:12 +0000

    small additions

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QWidget, QMainWindow
+from PySide6.QtGui import QIcon
 import sys
 
 print("Hello from PySide67")
 print("never mind no 67 here")
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi()
+
+    def setupUi(self):
+        windowIcon = QIcon()
+        windowIcon.addFile('assets/secure.png')
+        self.setWindowIcon(windowIcon)
+
 
 app = QApplication(sys.argv)
 
